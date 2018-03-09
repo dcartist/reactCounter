bae8759678e9df699bdbd7f799011c901be62813
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2018-03-09 15:14:15 -0500

    finalize starter

--- src/App.js
+++ src/App.js
@@ -3,13 +3,6 @@ import Header from "./Header";
 import CounterList from "./CounterList";
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      counters: 5
-    };
-  }
   render() {
     return (
       <div className="App">
@@ -17,15 +10,5 @@ class App extends Component {
         <CounterList counter={this.state.counters} />
       </div>
     );
-  }
-}
-
-export default App;
-
-class List extends Component {
-  render() {
-    let list = [<Item />, <Item />, <Item />];
-
-    return <div className="list">{list}</div>;
   }
 }
