6bac41c702d65970b10b1d93fdae5cdb53eacf4e
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2018-03-09 11:22:18 -0500

    update starter code

--- src/App.js
+++ src/App.js
@@ -1,15 +1,11 @@
 import React, { Component } from "react";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">React Counters</h1>
-          <button>+</button>
-          <button>-</button>
-        </header>
-        <div className="Counter-row" />
+        <Header />
       </div>
     );
   }
