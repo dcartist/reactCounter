b1c5dd5fbad7aa8ac24ab25fc8c24091e6c44790
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2018-03-09 15:15:28 -0500

    remove some scaffolding

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from "react";
 import Header from "./Header";
-import CounterList from "./CounterList";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <CounterList counter={this.state.counters} />
       </div>
     );
   }
