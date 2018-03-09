cdd044bd8f86f9cff1725de7d51c5166478baa3f
Zakk Fleischmann <fleischmann@teamsubjectmatter.com>
2018-03-09 10:37:51 -0500

    finish scaffolding

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
 import React, { Component } from "react";
-import Counter from "./Counter";
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      counters: 0
-    };
-
-    this.increaseCounters = this.increaseCounters.bind(this);
-    this.decreaseCounters = this.decreaseCounters.bind(this);
-  }
-
   render() {
-    let counters = [];
-    for (let index = 0; index < this.state.counters; index++) {
-      counters.push(<Counter key={index} counterNumber={index} />);
-    }
-
     return (
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">React Counters</h1>
-          <button onClick={this.increaseCounters}>+</button>
-          <button onClick={this.decreaseCounters}>-</button>
+          <button>+</button>
+          <button>-</button>
         </header>
-        <div className="Counter-row">{counters}</div>
+        <div className="Counter-row" />
       </div>
     );
-  }
-
-  increaseCounters() {
-    let counters = this.state.counters + 1;
-
-    this.setState({
-      counters
-    });
-  }
-
-  decreaseCounters() {
-    let counters = this.state.counters - 1;
-
-    this.setState({
-      counters
-    });
   }
 }
 
