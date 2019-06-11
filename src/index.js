5146193cb6c929f407afde57f0772e90866aa484
DCartist <pbannerman13@gmail.com>
2019-06-11 10:53:54 -0400

    working on skkeleton

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+//index is the top level of the file
+//can have react on only part of the page and not full page.
 
 const data = {
 	counters: 5
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App data={data}/>, document.getElementById("root"));
